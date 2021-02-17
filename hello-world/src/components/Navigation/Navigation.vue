71ac56a95b73cde828314cf2cc923c1e263bf758
Rajkamal <rajkamal.smit@gmail.com>
2021-02-17 12:40:35 +0530

    image

--- hello-world/src/components/Navigation/Navigation.vue
+++ hello-world/src/components/Navigation/Navigation.vue
@@ -1,9 +1,45 @@
 <template>
   <div class="mx-auto overflow-hidden" height="400">
-    <v-app-bar color="#070634">
+    <v-app-bar color="#070634" dark fixed>
       <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Title</v-toolbar-title>
+      <div>
+        <img
+          class="sunnyLogo"
+          src="https://cdn.shopify.com/s/files/1/0046/2140/2241/files/SUNNY_eSTORE_stacked_-white_60x.png?v=1590139908"
+        />
+      </div>
+
+      <div>
+        <svg
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+          ></path>
+        </svg>
+      </div>
+      <div>
+        <svg
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+          ></path>
+        </svg>
+      </div>
     </v-app-bar>
 
     <v-navigation-drawer v-model="drawer" absolute temporary>
@@ -32,6 +68,7 @@
 </template>
 
 <script>
+import "./Navigation.css";
 export default {
   data: () => ({
     drawer: false,
