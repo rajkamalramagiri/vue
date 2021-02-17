8c9f733c98bb945eacb1f49e1f69878d746964fc
Rajkamal <rajkamal.smit@gmail.com>
2021-02-17 13:50:34 +0530

    search bar

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,14 +1,22 @@
 <template>
-  <Navigation />
+  <div>
+    <Navigation />
+    <br />
+    <br />
+    <br />
+    <Search />
+  </div>
 </template>
 
 <script>
 import Navigation from "./components/Navigation/Navigation";
+import Search from "./components/Search/Search";
 export default {
   name: "App",
 
   components: {
     Navigation,
+    Search,
   },
 
   data: () => ({
---
+++ hello-world/src/components/Search/Search.vue
@@ -0,0 +1,16 @@
+<template>
+  <div style="background-color: blue">
+    <v-card>
+      <v-toolbar>
+        <v-text-field
+          prepend-icon="mdi-magnify"
+          label="Search"
+          solo-inverted
+          hide-details
+          clearable
+        ></v-text-field>
+      </v-toolbar>
+    </v-card>
+  </div>
+</template>
+
